578e61fa6ad0992167c268bed56a7cccff4ce43b
John Whitington <john@coherentgraphics.co.uk>
2013-10-15 18:36:39 +0100

    Work on tests

--- cpdflibtest.c
+++ cpdflibtest.c
@@ -1,11 +1,24 @@
 /* cpdflibtest.c */
 /* Uses every function in cpdflibwrapper.h */
 #include <stdio.h>
+#include <stdbool.h>
 #include "cpdflibwrapper.h"
 int main (int argc, char ** argv)
 {
+  setbuf(stdout, NULL);
+
+  printf("Set up OCaml runtime\n");
   caml_startup(argv);
+
+  printf("Set demo mode for commercial programs\n");
+  setdemo(false);
+
+  printf("Load from a file\n");
   int pdf = fromFile("logo.pdf");
+
+  printf("Write to a file\n");
   toFile(pdf, "out.pdf", 0, 0);
+
+  printf("*****Tests finished\n");
   return 0;
 }
