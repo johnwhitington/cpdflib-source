1f41115f95285f5a8ea11ee59f582b43d5fce0b3
John Whitington <john@coherentgraphics.co.uk>
2013-11-21 14:00:51 +0000

    Added simpletest.c

---
+++ simpletest.c
@@ -0,0 +1,41 @@
+
+#include <stdbool.h>
+#include "cpdflibwrapper.h"
+
+int main (int argc, char ** argv)
+{
+  /* Initialist cpdf. */
+  cpdf_startup(argv);
+
+  /* Clear the error state */
+  cpdf_clearError();
+
+  /* We will take the input hello.pdf and repeat it three times */
+  int mergepdf = cpdf_fromFile("hello.pdf", "");
+
+  /* Check the error state */
+  if lastError() return 1;
+
+  /* The array of PDFs to merge */
+  int pdfs[] = {mergepdf, mergepdf, mergepdf};
+
+  /* Clear the error state */
+  cpdf_clearError();
+
+  /* Merge them */
+  int merged = cpdf_mergeSimple(pdfs, 3);
+  
+  /* Check the error state */
+  if lastError() return 1;
+
+  /* Clear the error state */
+  clearError()
+
+  /* Write output */
+  cpdf_toFile(merged, "merged.pdf", false, false);
+
+  /* Check the error state */
+  if lastError() return 1;
+
+  return 0;
+}
