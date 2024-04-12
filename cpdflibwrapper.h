175de5f69caf0b66e51238b334794f9830885121
John Whitington <john@coherentgraphics.co.uk>
2024-04-13 07:53:43 +0800

    loadFont docs

--- cpdflibwrapper.h
+++ cpdflibwrapper.h
@@ -363,7 +363,12 @@ enum cpdf_encryptionMethod cpdf_encryptionKind(int);
 
 /* cpdf_loadFont(name, filename) loads a TrueType font from the given file
  * name, and names it. It may then be used when adding text or drawing, using
- * the name in place of a standard font name. */
+ * the name in place of a standard font name. NB: The font keeps a record of
+ * which glyphs are used, and outputs the appropriate subset to the PDF when it
+ * is written to disk or memory. Therefore, if you are using a font for
+ * multiple documents, you should re-load the font (under the same name is
+ * fine) to clear the list of used glyphs. Failure to to do so is not
+ * incorrect, but will result in larger subsets so is inefficient. */
 void cpdf_loadFont(char *, char *);
 
 /* CHAPTER 2. Merging and Splitting */
